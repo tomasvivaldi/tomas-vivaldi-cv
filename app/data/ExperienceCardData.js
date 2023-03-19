09e2e95a7d9f822bb23c876348bc449065c35218
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-03-19 19:33:59 +0800

    fixed build errors

--- app/data/ExperienceCardData.js
+++ app/data/ExperienceCardData.js
@@ -56,12 +56,11 @@ export const ExperienceCardData = [
       "Game's DeFi Logic Administrator",
       "Back-end Code with UE5 and AWS Databases",
     ],
-    jobDescription1: [
+    jobDescription1:
       "I played a significant role in the back-end development team at Tokyo Creation that produced the first blockchain-based first-person shooter game in Japan.  Because of my experience and the small team at the company,  I was also given other duties, such as web design and development utilizing Nextjs, React, and Tailwind for our startup's investor's company.",
-    ],
-    jobDescription2: [
+
+    jobDescription2:
       "I also served as the DeFi Logic Administrator for the game and designed its UI/UX  using Adobe Applications, as well as UE5. I also worked on the back-end code for the game, using both UE5 and AWS Databases.",
-    ],
   },
   {
     img: "/images/ArkhonTech.svg",
@@ -92,12 +91,11 @@ export const ExperienceCardData = [
       "Created documentation for various aspects of software development",
       "CI/CD Pipelines",
     ],
-    jobDescription1: [
+    jobDescription1:
       "As a co-founder of Archon Tech,  which specialized in automating trading methods for both traditional markets and digital assets. My responsibilities encompassed maintaining databases, making sure that client and trading data was safely maintained, and integrating APIs to make it easier for our software to communicate with other platforms such as Binance. ",
-    ],
-    jobDescription2: [
+
+    jobDescription2:
       "I wrote the programming that enabled our software to automatically execute transactions in accordance with established rules by translating sophisticated trading methods. In order to ensure our team could work effectively, I also prepared documentation for several parts of software development. In order to continually test, develop, and deploy our software to production environments, I also established and managed CI/CD pipelines.",
-    ],
   },
   {
     img: "/images/bosch.jpg",
@@ -142,12 +140,10 @@ export const ExperienceCardData = [
       "Database Integrations",
       "Self-Driving-Car Rout Programing",
     ],
-    jobDescription1: [
+    jobDescription1:
       "I was a participant in the creation of Bosch's self-driving car software. I worked on the route programming for self-driving cars, developing algorithms to make sure the vehicle took the correct paths and avoided hazards along the way. In addition to that, I was in charge of creating a graphical user interface (GUI) in order to visualize the self-driving-car's programmed route.",
-    ],
-    jobDescription2: [
+    jobDescription2:
       "In order to ensure that data was being stored and retrieved accurately and effectively, I additionally managed database integrations. In order to guarantee continuous connection between the software and the car's hardware, I worked on Kubernetes integrations as part of this task.",
-    ],
   },
   {
     img: "/images/shopify.svg",
@@ -171,12 +167,11 @@ export const ExperienceCardData = [
       "SEO Optimization",
       "Marketing",
     ],
-    jobDescription1: [
+    jobDescription1:
       "As the owner of an online store specializing in men's wear and accessories, I was in charge of every part of the company, including marketing, database maintenance, SEO optimization, and website UI/UX design. To ensure that clients had a great buying experience, I made use of my website design expertise to construct a storefront that was both aesthetically pleasing and user-friendly. In order to guarantee that client information and sales statistics were correct and current, I also monitored and maintained the databases connected to the business.",
-    ],
-    jobDescription2: [
+
+    jobDescription2:
       "I was able to raise the store's search engine rating and improve revenue through my marketing and SEO optimization efforts. I successfully reached and engaged with our consumer base by creating and implementing targeted advertising campaigns.",
-    ],
   },
   {
     img: "/images/freelance.png",
@@ -220,11 +215,9 @@ export const ExperienceCardData = [
       "SEO Optimization",
       "Implementation of Responsive Design Principles",
     ],
-    jobDescription1: [
+    jobDescription1:
       "My career in software engineering began when I teamed up with a colleague to provide clients with specialized website building services through social media outreach and web marketing strategies. I started off as a freelance developer concentrating on front-end web development, making aesthetically pleasing and simple interfaces.",
-    ],
-    jobDescription2: [
+    jobDescription2:
       "I soon understood that adding back-end programming was essential to providing a seamless user experience. I improved my abilities to become a full-stack developer, adept in both front-end and back-end development, to create comprehensive solutions.",
-    ],
   },
 ];
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import WorkExperience from "./components/WorkExperience";
 import Skills from "./components/Skills";
-import Projects from "./components/Projects";
+// import Projects from "./components/Projects";
 import ContactMe from "./components/ContactMe";
 
 const inter = Inter({ subsets: ["latin"] });
