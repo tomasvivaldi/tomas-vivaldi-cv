6c228bad1a7339346487d033b7fd140ab7659233
tomasvivaldi <antonio.tomas.vivaldi@gmail.com>
2023-03-22 12:00:27 +0800

    Finished Experience section

--- app/components/ExperienceCard.tsx
+++ app/components/ExperienceCard.tsx
@@ -43,138 +43,103 @@ const ExperienceCard = ({
   }, []);
 
   return (
-    <AnimatePresence>
-      {expanded ? (
-        <Backdrop
-          onClick={() => {
-            console.log("Backdrop clicked");
-            setExpanded(!expanded);
-          }}
-        ></Backdrop>
-      ) : null}
-      <motion.article
-        onClick={(e: React.MouseEvent<HTMLDivElement>) => {
-          console.log(" clicked");
-          setExpanded(!expanded);
-          e.stopPropagation();
-        }}
-        whileTap={{
-          scale: 0.95,
-        }}
-        style={{
-          width: expanded ? 300 : "auto",
-          height: expanded ? 700 : "auto",
-          overflowY: expanded ? "scroll" : "hidden",
-          background: expanded ? "#262626" : "auto",
-          opacity: expanded ? 1 : "auto",
-          z: expanded ? 40 : 0,
-        }}
-        animate={{
-          scale: expanded ? 1.2 : 1,
-          position: expanded ? "absolute" : "static",
-          top: expanded ? "0" : "auto",
-        }}
-        className="relative flex flex-col py-6 gap-3 rounded-lg items-center flex-shrink-0 
-    w-full h-full md:w-[600px] xl:w-[900px] snap-center bg-gray-400/20
+    <motion.article
+      className="relative flex flex-col py-3 sm:py-6 gap-3 rounded-lg items-center flex-shrink-0 
+        h-full md:w-[600px] xl:w-[900px] snap-center bg-gray-400/20
      transition-opacity duration-200 overflow-hidden px-3 overflow-y-auto
      opacity-60 hover:opacity-100"
-      >
-        <img
-          className="w-[320px] h-[120px] xl:w-[420px] xl:h-[200px] rounded-xl object-cover"
-          src={img}
-          alt="image"
-        />
-        <div className="px-3 md:px-10 space-y-4 mx-1">
-          <h4 className=" text-xl md:text-4xl font-light">{title}</h4>
-          <p className=" text-xs md:text-lg font-bold uppercase">{subtitle}</p>
+      style={{ width: "calc(100vw - 4rem)" }}
+    >
+      <img
+        className="w-[320px] h-[120px] xl:w-[420px] xl:h-[200px] rounded-xl object-cover"
+        src={img}
+        alt="image"
+      />
+      <div className="sm:px-3 md:px-10 space-y-2 md:space-y-4 mx-1">
+        <h4 className="text-lg md:text-4xl font-light">{title}</h4>
+        <p className="text-md font-bold uppercase">{subtitle}</p>
 
-          {isMobile ? (
-            <div className="flex my-2 space-x-2 mx-auto justify-center flex-wrap">
-              {techstack.map((tech, index) => (
+        {isMobile ? (
+          <div className="flex my-1 ml-2 space-x-2 mx-auto justify-center flex-wrap">
+            {techstack.map((tech, index) => (
+              <Image
+                key={index}
+                className="rounded-xl items-center justify-center w-[25px] h-[25px] my-1 ml-1 mr-1"
+                src={tech.src}
+                alt="image"
+                width={25}
+                height={25}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className="flex my-2 space-x-2 ml-2 flex-wrap">
+            {techstack.map((tech, index) => (
+              <a key={index} href={tech.url} target="_blank">
                 <Image
                   key={index}
-                  className="rounded-xl items-center justify-center w-[25px] h-[25px] my-1 ml-1 mr-1"
+                  className="rounded-xl w-[25px] h-[25px] m-1"
                   src={tech.src}
                   alt="image"
                   width={25}
                   height={25}
                 />
-              ))}
-            </div>
-          ) : (
-            <div className="flex my-2 space-x-2 ml-2 flex-wrap">
-              {techstack.map((tech, index) => (
-                <a key={index} href={tech.url} target="_blank">
-                  <Image
-                    key={index}
-                    className="rounded-xl w-[25px] h-[25px] m-1"
-                    src={tech.src}
-                    alt="image"
-                    width={25}
-                    height={25}
-                  />
-                </a>
-              ))}
-            </div>
-          )}
-
-          <div className=" space-x-6 flex ml-6 text-gray-300">
-            <p className=" font-extralight text-left">
-              Started work: {startDate}
-            </p>
-            <p className=" font-extralight text-left">Ended: {endDate}</p>
-          </div>
-
-          <ul
-            className="list-disc list-inside space-y-2 ml-6 
-        text-base mx-auto my-0 text-left "
-          >
-            {summaries.map((summary, index) => (
-              <li
-                key={index}
-                dangerouslySetInnerHTML={{ __html: summary }}
-              ></li>
+              </a>
             ))}
-          </ul>
-        </div>
-        <div
-          className=" absolute group right-0 top-0 w-9 h-9 rounded-md bg-gray-300/[0.8] 
-      backdrop-blur-sm animate-pulse hover:animate-none hover:h-full hover:w-full
-       ease-in-out transition-all duration-500 md:m-3"
-        >
-          <div className=" group-hover:hidden absolute bottom-0 left-0 h-4 w-4  m-2 border-b-2 border-l-2 border-b-red-500 border-l-red-500" />
-          <div className=" group-hover:hidden absolute bottom-0 left-0 h-3 w-3  m-3 border-b-2 border-l-2 border-b-red-500 border-l-red-500" />
-          <motion.div
-            initial={{
-              y: -100,
-              opacity: 0,
-            }}
-            whileHover={{
-              y: 0,
-              opacity: [0, 0, 0, 0.5, 1],
-            }}
-            transition={{ duration: 1 }}
-            viewport={{ once: false }}
-            className="h-full w-full hidden group-hover:flex flex-col text-black gap-3"
-          >
-            <div
-              className="hidden group-hover:flex text-left font-light
-           text-base m-4 flex-col gap-8 h-full"
-            >
-              <h3
-                className="hidden group-hover:flex flex-nowrap justify-center mt-9 tracking-widest text-center font-semibold  px-1
-          text-2xl "
-              >
-                What was I responsible for:
-              </h3>
+          </div>
+        )}
 
-              <p className="text-xl indent-9 text-justify">{jobDescription1}</p>
-              <p className="text-xl indent-9 text-justify">{jobDescription2}</p>
+        <div className="font-extralight text-left text-sm space-x-6 flex ml-2 sm:ml-6 text-gray-300">
+          <p className="">Start: {startDate}</p>
+          <p className="">End: {endDate}</p>
+        </div>
+
+        <ul
+          className="list-disc list-inside space-y-2 ml-2 sm:ml-6 
+        text-base mx-auto my-0 text-left "
+        >
+          {summaries.map((summary, index) => (
+            <li key={index} dangerouslySetInnerHTML={{ __html: summary }}></li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        {expanded ? (
+          <motion.div
+            onClick={() => {
+              console.log("clicked");
+              setExpanded(!expanded);
+            }}
+            transition={{ duration: 1.5 }}
+          >
+            <div className="absolute group right-0 top-0 rounded-md bg-black/[0.8] backdrop-blur-lg animate-none min-h-full h-fit w-full ease-in-out transition-all duration-500 md:m-3">
+              <div className="text-left font-light text-base m-4 flex-col flex gap-3 h-full overflow-y-scroll overflow-x-clip">
+                <h3 className="flex-nowrap justify-center mt-3 md:mt-9 tracking-widest text-center text-lg -mx-2 font-semibold px-1 md:text-2xl">
+                  What was I responsible for:
+                </h3>
+                <p className="md:text-xl indent-9 text-justify mx-3">
+                  {jobDescription1}
+                </p>
+                <p className="md:text-xl indent-9 text-justify mx-3">
+                  {jobDescription2}
+                </p>
+              </div>
             </div>
           </motion.div>
-        </div>
-      </motion.article>
-    </AnimatePresence>
+        ) : (
+          <div
+            onClick={() => {
+              console.log("clicked");
+              setExpanded(!expanded);
+            }}
+            className="absolute group right-0 top-0 w-9 h-9 rounded-md bg-gray-400/[0.8] backdrop-blur-md animate-pulse ease-in-out transition-all duration-500 md:m-3"
+          >
+            <div className="absolute bottom-0 left-0 h-4 w-4 m-2 border-b-2 border-l-2 border-b-red-500 border-l-red-500" />
+            <div className="absolute bottom-0 left-0 h-3 w-3 m-3 border-b-2 border-l-2 border-b-red-500 border-l-red-500" />
+          </div>
+        )}
+      </div>
+    </motion.article>
   );
 };
 
--- app/components/WorkExperience.tsx
+++ app/components/WorkExperience.tsx
@@ -9,9 +9,9 @@ export default function WorkExperience() {
 
   return (
     <motion.div
-      className="h-[700px] overflow-hidden flex flex-col text-center
-md:text-left  max-w-7xl md:px-10 justify-evenly
-mx-auto items-center pt-6"
+      className="h-[calc(100vh - 3rem)] overflow-hidden flex flex-col text-center
+      md:text-left  max-w-7xl md:px-10 justify-evenly relative
+      mx-auto items-center pt-6"
       initial={{
         opacity: 0,
       }}
@@ -26,7 +26,7 @@ mx-auto items-center pt-6"
       </h3>
 
       <div
-        className="w-full h-[600px] flex space-x-5 overflow-x-scroll overflow-y-hidden
+        className="w-full h-[580px] flex space-x-5 overflow-x-scroll overflow-y-hidden
        px-10 py-4 snap-x snap-mandatory scrollbar-thin
        scrollbar-track-gray-400/20 scrollbar-thumb-red-500/50 "
       >
